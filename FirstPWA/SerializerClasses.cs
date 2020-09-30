c3102364d472dab63f8bfde4dd61857dd678696f
ststeiger <quandary82@hailmail.net>
2020-09-30 17:25:39 +0200

    Added sources

--- FirstPWA/SerializerClasses.cs
+++ FirstPWA/SerializerClasses.cs
@@ -2,6 +2,20 @@
 namespace FirstPWA
 {
 
+    // https://docs.microsoft.com/en-us/aspnet/core/migration/http-modules?view=aspnetcore-3.1
+    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/routing?view=aspnetcore-3.1
+
+
+    // https://stackoverflow.com/questions/38632723/what-can-service-workers-do-that-web-workers-cannot
+
+
+    // https://itprosteer.com/how-long-does-a-pwa-development-project-take/#:~:text=Summary%3A,4%20weeks%20%2D%20discovery
+    // https://codelabs.developers.google.com/codelabs/your-first-pwapp/#1
+    // https://developers.google.com/web/ilt/pwa/live-data-in-the-service-worker
+    // from https://github.com/googlecodelabs/your-first-pwapp 
+    // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps
+    // https://web.dev/progressive-web-apps/
+
 
     public partial class Temperatures
     {
